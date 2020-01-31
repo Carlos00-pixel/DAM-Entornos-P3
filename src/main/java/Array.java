eda7b3f5823b9107e5c1f3b9301180f729ae2366
alexs <alexs@LAPTOP-KQGHEVBO>
2020-01-31 21:08:10 +0100

    minimo

--- src/main/java/Array.java
+++ src/main/java/Array.java
@@ -1,30 +1,23 @@
 package main.java;
 
 public class Array {
-	
-	 public static void mediana(int[] array){
-	        double mediana = 0;
-	        if(array.length % 2 == 0){
-	            int sumaMedios = array[array.length/2] + array[array.length/2 - 1]; 
-	            mediana = (double)sumaMedios / 2; 
-	        }
-	        
-	        System.out.println(mediana);
-	        
-	    }
+	public static void minimo(int []minimo) {
+		int numeroMinimo=999;
+		for(int i=0; i<minimo.length; i++) {
+			if(minimo[i]<numeroMinimo) {
+				numeroMinimo=minimo[i];
+			}
+		}
+		System.out.println("El numero minimo es "+numeroMinimo);
+	}
 
 	public static void main(String[] args) {
 		
 ///////////////////////////////////////  NO HACER NADA AQUI ///////////////////////////////////////////
 		
-		int[] maximo = {3,7,5,9,6};
 
 		int[] minimo = {3,7,5,9,6};
-
-		int[] mediana = {5,7,45,20,70,26};
-		mediana(mediana);
-
-		int[] media = {4,50,8,20};
+		minimo(minimo);
 	}
 
 }
