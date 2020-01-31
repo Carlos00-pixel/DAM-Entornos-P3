d4b40e53c41c381dfeaa38460a33b636d92e7b6d
DAM_1 <DAM_1@PB01>
2020-01-31 20:47:29 +0100

    Mejora3

--- src/main/java/Array.java
+++ src/main/java/Array.java
@@ -1,6 +1,17 @@
 package main.java;
 
 public class Array {
+	
+	 public static void mediana(int[] array){
+	        double mediana = 0;
+	        if(array.length % 2 == 0){
+	            int sumaMedios = array[array.length/2] + array[array.length/2 - 1]; 
+	            mediana = (double)sumaMedios / 2; 
+	        }
+	        
+	        System.out.println(mediana);
+	        
+	    }
 
 	public static void main(String[] args) {
 		
@@ -11,6 +22,7 @@ public class Array {
 		int[] minimo = {3,7,5,9,6};
 
 		int[] mediana = {5,7,45,20,70,26};
+		mediana(mediana);
 
 		int[] media = {4,50,8,20};
 	}
