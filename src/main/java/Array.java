79a1f88c5fb4180c7a8281ae64168fd3027204de
Carlos <Carlos@CARLOS>
2020-02-07 19:52:22 +0100

    Mejoras2,3

--- src/main/java/Array.java
+++ src/main/java/Array.java
@@ -1,6 +1,7 @@
 package main.java;
 
 public class Array {
+	
 	public static void minimo(int []minimo) {
 		int numeroMinimo=999;
 		for(int i=0; i<minimo.length; i++) {
@@ -11,13 +12,31 @@ public class Array {
 		System.out.println("El numero minimo es "+numeroMinimo);
 	}
 
+	 public static void mediana(int[] array){
+	        double mediana = 0;
+	        if(array.length % 2 == 0){
+	            int sumaMedios = array[array.length/2] + array[array.length/2 - 1]; 
+	            mediana = (double)sumaMedios / 2; 
+	        }
+	        
+	        System.out.println(mediana);
+	        
+	    }
+	 
 	public static void main(String[] args) {
 		
 ///////////////////////////////////////  NO HACER NADA AQUI ///////////////////////////////////////////
 		
+		int[] maximo = {3,7,5,9,6};
 
 		int[] minimo = {3,7,5,9,6};
+
+		int[] mediana = {5,7,45,20,70,26};
+
+		int[] media = {4,50,8,20};
+		
 		minimo(minimo);
+		mediana(mediana);
 	}
 
 }
