95147412b73efb33d50b01d3ce6e927ef6df2268
alexs <alexs@LAPTOP-KQGHEVBO>
2020-02-07 19:13:48 +0100

    hola

--- src/main/java/Numero.java
+++ src/main/java/Numero.java
@@ -3,28 +3,6 @@ package main.java;
 import java.util.Scanner;
 
 public class Numero {
-	public static void primos(int desde, int hasta) {
-		boolean esPrimo;
-		for(int i =desde; i<=hasta; i++ ) {
-			esPrimo=true;
-			for(int j=2; j<=Math.sqrt(i)&& esPrimo; j++) {
-				if(i% j==0) {
-					
-				}
-			}
-			if(esPrimo) {
-				System.out.print(i+"-");
-			}
-		}
-	}
-	
-	public static void factorial(int numero, int i, int factorial) {
-		do {
-			factorial=factorial*i;
-			i++;
-		}while(i<=numero);
-			System.out.println("El numero es "+factorial);
-	}
 	 public static void main(String[] args) {
 		 
 ///////////////////////////////////////  NO HACER NADA AQUI ///////////////////////////////////////////
@@ -41,10 +19,9 @@ public class Numero {
 	    int numero;
 	    int i=1;
 		int factorial = 1;
-		primos(desde,hasta);
 		System.out.println("");
 		System.out.println("Introduce un numero: ");
 		numero = scanner.nextInt();
-		factorial(numero, i, factorial);
+
 	 }
 }
