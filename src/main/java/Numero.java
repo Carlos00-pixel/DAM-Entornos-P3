3c4828ecbe64f9432fb614245f79843c48a3bb4b
alexs <alexs@LAPTOP-KQGHEVBO>
2020-02-07 19:16:21 +0100

    mejora5

--- src/main/java/Numero.java
+++ src/main/java/Numero.java
@@ -3,6 +3,20 @@ package main.java;
 import java.util.Scanner;
 
 public class Numero {
+	public static void primos(int desde, int hasta) {
+		boolean esPrimo;
+		for(int i =desde; i<=hasta; i++ ) {
+			esPrimo=true;
+			for(int j=2; j<=Math.sqrt(i)&& esPrimo; j++) {
+				if(i% j==0) {
+					
+				}
+			}
+			if(esPrimo) {
+				System.out.print(i+"-");
+			}
+		}
+	}
 	 public static void main(String[] args) {
 		 
 ///////////////////////////////////////  NO HACER NADA AQUI ///////////////////////////////////////////
@@ -19,6 +33,7 @@ public class Numero {
 	    int numero;
 	    int i=1;
 		int factorial = 1;
+		primos(desde, hasta);
 		System.out.println("");
 		System.out.println("Introduce un numero: ");
 		numero = scanner.nextInt();
